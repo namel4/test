a3a8506dc83cd2abd3307cad42da2b33304fbd6b
zhaotianqi02 <zhaotianqi02@meituan.com>
2021-08-31 15:57:00 +0800

    哈

--- hello.java
+++ hello.java
@@ -1,8 +1,30 @@
+import java.util.Arrays;
+
 class test{
-	public static void mian(String arg){
-		System.out.println("zhong  hua  ren ming  gong  he  guo ");
-		//写个for循环
-		//需要查下资料吗？
+	public static void main(String args[]){
+		System.out.println("德玛西亚");
+		int[] arr = {2,4,1,9,6,8,7,10,3,5};
+
+		bubbleSort(arr);
+		System.out.println(Arrays.toString(arr));
 		
 	}
+
+	private static void bubbleSort(int[] arr){
+		if(arr == null || arr.length < 2){
+			return;
+		}else {
+			for (int i=0;i<arr.length-1;i++){
+				for (int j=0;j<arr.length-1-i;j++){
+					if(arr[j] > arr[j+1]){
+						int temp = arr[j];
+						arr[j] = arr[j+1];
+						arr[j+1] = temp;
+					}
+				}
+			}
+		}
+	}
+
+
 }
